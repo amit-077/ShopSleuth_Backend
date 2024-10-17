ebb0e5f953127bd95b998d56bb35593d668963c0
amit-077 <amitkamat918@gmail.com>
2024-10-18 02:09:42 +0530

    change1

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 connectDB();
 
 const corsOptions = {
-  origin: ["*"], // Restrict to your front-end domain
+  origin: "*", // Restrict to your front-end domain
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true, // Allow credentials (cookies, HTTP authentication)
